5b899ab0dc43f9b0bc4d7dd2f708aae12b525e1d
dibamirza <dimirza@cs.ucsb.edu>
2024-09-29 16:10:42 -0700

    lect1- drawing with turtle

---
+++ lect1/turtle_drawing.py
@@ -0,0 +1,48 @@
+# Program to draw CS8 using turtle graphics module
+# To run this program, first download and install
+# Python: https://www.python.org/downloads/
+# It comes with an integrated development environment (IDLE)
+# In class, I gave a demo of how to run programs in IDLE
+
+import turtle #Needed to use the turtle module
+
+# get a pen to draw with
+jane = turtle.Turtle()
+jane.shape("turtle")
+jane.width(5)
+jane.left(180)
+jane.forward(100)
+jane.left(90)
+jane.forward(100)
+jane.left(90)
+jane.forward(100)
+## jane.left(90)
+##jane.forward(100)
+##jane.left(90)
+jane.forward(120)
+jane.left(90)
+jane.forward(50)
+jane.left(90)
+jane.forward(100)
+jane.right(90)
+jane.forward(50)
+jane.right(90)
+jane.forward(100)
+jane.forward(120)
+jane.right(90)
+jane.forward(100)
+jane.right(90)
+jane.forward(100)
+jane.right(90)
+jane.forward(50)
+jane.right(90)
+jane.forward(100)
+jane.backward(100)
+jane.left(90)
+jane.forward(50)
+jane.right(90)
+jane.forward(100)
+
+
+
+
