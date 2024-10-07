295b60e29f256ae316169c4a3bf621ed2436b0ab
Diba Mirza <dimirza@cs.ucsb.edu>
2024-10-07 11:31:23 -0700

    Create lect4.py

---
+++ lect4.py
@@ -0,0 +1,18 @@
+## Lecture 4 - Zybook 2.5, 2.8 – 2.11, 2.19
+# Big picture of using functions in code
+# 1. Given the description of a problem in English, write the definition of a function that solves the problem
+
+# 2. Given a monolithic block of code, use functions to make it more modular/readable: refactoring code
+
+# 3. Given a complex problem (like the project), figure out how to decompose the problem 
+# what functions you actually need to solve this larger problem
+
+# Last lecture, we made the following claims about functions:
+# 1.Functions make it easier to test code
+# 2.Functions make it easier to find/fix program errors (bugs)
+# 3.Functions make it easier to write code (come up with the right algorithm)
+
+# Main goals of today's lecture: 
+# 1. Demonstrate the above claims by coding an example game: FizzBuzz
+# 2. Demonstrate how to incrementally develop code/test code using functions
+#  
