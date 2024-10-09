dc49f8b499e49f6a5f254c99368582c81232b70c
Diba Mirza <dimirza@cs.ucsb.edu>
2024-10-09 16:38:31 -0700

    Create lect5.py

---
+++ lect5.py
@@ -0,0 +1,6 @@
+# Lecture 5
+# Problem: "Triangle Type Checker"
+# Problem Statement:
+# Write a program that determines the type of a triangle (Equilateral, Isosceles, or Scalene)  based on the lengths of its sides. 
+# The program should also check whether the three sides can actually 
+# form a valid triangle using the Triangle Inequality Theorem (any two sides of a triangle must be greater than the third side).
