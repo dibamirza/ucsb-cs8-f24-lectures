ecb9bfabdaa0eef463589d2d75bf69084bb91463
dibamirza <dimirza@cs.ucsb.edu>
2024-10-24 08:43:51 -0700

    lect 9: nested lists and loops

---
+++ lect9/asciiart.py
@@ -0,0 +1,36 @@
+# Nested Loops, practice problems with ASCII Art
+# # 1. Draw a rectangle, given its width and height
+# # 2. Draw the letter C, given its width and height
+
+# Review of the accumulator pattern
+
+def message():
+    '''What does this function return?'''
+    sentence_list = ['Hello', 
+                     'fellow coder!',
+                     'Remember to take short breaks when coding!']
+    result = ""
+    for elem in sentence_list:
+        result += elem + "\n"
+    
+    return result
+
+print(message())
+
+# 1. Write a function that returns a rectangle of *'s', given its width and height
+def getRectangle(width, height):
+  '''returns a string representing a 
+     rectangle of *s with given width and height'''
+  return ""
+
+
+print(getRectangle(3, 4))
+
+
+
+# 2. Write a function that returns a string representing the letter C, given its width and height
+def getC(width, height):
+  '''returns a string representing the letter C 
+     using *s with given width and height'''
+  
+  return ""
---
+++ lect9/tictactoe.py
@@ -0,0 +1,12 @@
+# Lecture 9: Tic Tac Toe
+
+
+def print_board(board):
+  '''prints the board in the following format
+     X | . | O 
+    ---+---+---
+     O | . | .  
+    ---+---+---
+     . | . | X 
+  '''
+  return
